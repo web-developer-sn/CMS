b61e12fd3d58144911887ba6f5eda1d4aa03c000
satendra <web.developer.sn@gmail.com>
2026-03-06 18:55:59 +0530

    after adding backend for revers proxy

--- client/next.config.ts
+++ client/next.config.ts
@@ -5,6 +5,15 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV,
   },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
